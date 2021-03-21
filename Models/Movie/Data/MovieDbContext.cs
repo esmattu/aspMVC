0af7582d2575edf307d289720f66b845d6c175a2
Eishpal Mattu <eish007@hotmail.com>
2021-03-21 23:41:02 +0000

    added the scoffold item and entitiy framework

---
+++ Models/Movie/Data/MovieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using aspMVC.Models;
+
+namespace aspMVC.Models.Movie.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
